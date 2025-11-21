f659d22540236144dab4cb9ba12658cff93aa04f
AlexLeesman <aleesman315@gmail.com>
2025-11-21 10:07:28 -0600

    Created seperate py file for calculating pi

---
+++ calculatepifunction.py
@@ -0,0 +1,19 @@
+#Define a square region with sides of a certain length. In that region define a circle with a radius of your choosing.
+#Monte Carlo Technique
+
+def area(x):
+    #bounds of the square in both x and y directions
+    a = 0
+    b = x
+    #circle of radius 5, centered at (0,0)
+
+    n = 10000000
+    pointsinside = 0
+    for i in range(n):
+        point = (rand.uniform(a,b),rand.uniform(a,b))
+        if point[0]**2 + point[1]**2 <= x**2:
+            pointsinside += 1
+    #times 4 since the region is a quarter circle
+    area = (pointsinside/n)*4
+
+    return area
